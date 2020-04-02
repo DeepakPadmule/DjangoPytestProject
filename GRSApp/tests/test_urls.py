a576d4f5bb9eb4cfac3260c033d8199a1aec620f
Deepak <deepak.padmule@afourtech.com>
2020-04-02 18:45:54 +0530

    Fourt Commit

--- GRSApp/tests/test_urls.py
+++ GRSApp/tests/test_urls.py
@@ -1,28 +1,42 @@
+import mock
 from django.urls import reverse, resolve
 
 
 class TestUrls():
 
-    def test_single_grievance_type_url(self):
-        path = reverse('single_grievance_type', kwargs={'pk': "GT-0001"})
-        assert resolve(path).view_name == 'single_grievance_type'
+    def get_path(self):
+        # path = reverse('single_grievance_type', kwargs={'pk': "GT-0001"})
+        # print('get_path called......')
+        # print(path)
+        # return path
+        pass
 
-    def test_view_all_grievance_types_url(self):
-        path = reverse('grievance_types')
-        assert resolve(path).view_name == 'grievance_types'
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_single_grievance_type_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievanceType/GT-0001'
+        assert resolve(self.get_path()).view_name == 'single_grievance_type'
 
-    def test_single_grievance_url(self):
-        path = reverse('single_grievance', kwargs={'pk': "G-0001"})
-        assert resolve(path).view_name == 'single_grievance'
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_view_all_grievance_types_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievanceTypes'
+        assert resolve(self.get_path()).view_name == 'grievance_types'
 
-    def test_view_all_grievances_url(self):
-        path = reverse('grievances')
-        assert resolve(path).view_name == 'grievances'
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_single_grievance_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievance/G-0001'
+        assert resolve(self.get_path()).view_name == 'single_grievance'
 
-    def test_single_grievance_detail_url(self):
-        path = reverse('single_grievance_detail', kwargs={'pk': "GD-0001"})
-        assert resolve(path).view_name == 'single_grievance_detail'
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_view_all_grievances_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievances'
+        assert resolve(self.get_path()).view_name == 'grievances'
 
-    def test_view_all_grievance_details_url(self):
-        path = reverse('grievance_details')
-        assert resolve(path).view_name == 'grievance_details'
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_single_grievance_detail_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievanceDetail/GD-0001'
+        assert resolve(self.get_path()).view_name == 'single_grievance_detail'
+
+    @mock.patch('GRSApp.tests.test_urls.TestUrls.get_path')
+    def test_view_all_grievance_details_url(self, mock_get_path):
+        mock_get_path.return_value = '/grievanceDetails'
+        assert resolve(self.get_path()).view_name == 'grievance_details'
--- GRSApp/tests/test_views.py
+++ GRSApp/tests/test_views.py
@@ -2,78 +2,84 @@ import pytest
 
 from django.urls import reverse
 from rest_framework import status
+from django.test import Client
 
-from ..factories import GrievanceTypesFactory, GrievancesFactory, GrievanceDetailsFactory
+from ..factories import GrievanceTypesFactory, \
+                        GrievancesFactory, \
+                        GrievanceDetailsFactory
 
 
 @pytest.fixture
-def grievance_types_fixture():
+def create_grievance_types_using_fixture():
     GrievanceTypesFactory.create_batch(10)
 
 
 @pytest.fixture
-def grievances_fixture():
+def create_grievances_using_fixture():
     GrievancesFactory.create_batch(10)
 
 
 @pytest.fixture
-def grievance_details_fixture():
+def create_grievance_details_using_fixture():
     GrievanceDetailsFactory.create_batch(10)
+
+
+client = Client()
 
 
 @pytest.mark.django_db
 class TestViews():
 
-    def test_valid_single_grievance_type(self, client, grievance_types_fixture):
-        grievance_types_fixture
+    def test_valid_single_grievance_type(self, create_grievance_types_using_fixture):
+        create_grievance_types_using_fixture
         path = reverse('single_grievance_type', kwargs={'pk': "GT-0001"})
         response = client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_invalid_single_grievance_type(self, client, grievance_types_fixture):
-        grievance_types_fixture
+    def test_invalid_single_grievance_type(self, create_grievance_types_using_fixture):
+        create_grievance_types_using_fixture
         path = reverse('single_grievance_type', kwargs={'pk': "GT-0111"})
         response = client.get(path)
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_all_grievance_types(self, client, grievance_types_fixture):
-        grievance_types_fixture
+    def test_all_grievance_types(self, create_grievance_types_using_fixture):
+        create_grievance_types_using_fixture
         path = reverse('grievance_types')
         response = client.get(path)
         print(response.data)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_valid_single_grievance(self, client, grievances_fixture):
-        grievances_fixture
+    def test_valid_single_grievance(self, create_grievances_using_fixture):
+        create_grievances_using_fixture
         path = reverse('single_grievance', kwargs={'pk': "G-0001"})
         response = client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_invalid_single_grievance(self, client, grievances_fixture):
-        grievances_fixture
+    def test_invalid_single_grievance(self, create_grievances_using_fixture):
+        create_grievances_using_fixture
         path = reverse('single_grievance', kwargs={'pk': "G-0111"})
         response = client.get(path)
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_all_grievances(self, client, grievances_fixture):
-        grievances_fixture
+    def test_all_grievances(self, create_grievances_using_fixture):
+        create_grievances_using_fixture
         path = reverse('grievances')
         response = client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_single_grievance_detail(self, client, grievance_details_fixture):
-        grievance_details_fixture
+    def test_single_grievance_detail(self, create_grievance_details_using_fixture):
+        create_grievance_details_using_fixture
         path = reverse('single_grievance_detail', kwargs={'pk': "GD-0001"})
         response = client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_all_grievance_details(self, client, grievance_details_fixture):
-        grievance_details_fixture
+    def test_all_grievance_details(self, create_grievance_details_using_fixture):
+        create_grievance_details_using_fixture
         path = reverse('grievance_details')
         response = client.get(path)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_post_grievance_type(self, client):
+    def test_post_grievance_type(self):
         path = reverse('grievance_types')
         griev_data = {
             "griev_type": "Road"
@@ -81,7 +87,7 @@ class TestViews():
         response = client.post(path, data=griev_data)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_valid_post_grievance(self, client):
+    def test_valid_post_grievance(self):
         path = reverse('grievances')
         griev_data = {
             "griev_title": "Road Issue",
@@ -90,7 +96,7 @@ class TestViews():
         response = client.post(path, data=griev_data)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_invalid_post_grievance(self, client):
+    def test_invalid_post_grievance(self):
         path = reverse('grievances')
         griev_data = {
             "griev_title": "",
